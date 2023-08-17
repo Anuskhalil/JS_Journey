38b599fc6c32464d3e8f3662c52db12887c99be9
Anus Khalil <anuskhalil77@gmail.com>
2023-08-17 22:29:31 +0500

    FEAK: JavaScript Exploring

--- app.js
+++ app.js
@@ -1,11 +1,97 @@
-//alert
-alert("Hello World");  //both We can us
-window.alert("Hello World");
+// alert
+// alert("Hello World");  //both We can us
+// window.alert("Hello World");
 
 
 // Variable
 
+// ES5 => var ES6 => let and const 
+// var myApp = 'Anus khalil'
+// // console.log(myApp)
+// document.write(myApp)
 
+// DataTypes
+// 1: string
+// 2: number
+// 3: Boolean
+// 4: Integer
+// 6: Symbol
+
+// var myNum = 5;
+// console.log(myNum);
+
+
+// var myName = "Anus"
+// var myNum = 5;
+
+// console.log(myName + myNum); //concatenation
+
+// var myName = "Anus"
+// var myId = 007
+// console.log("My name is "+ myName + " and My Id is "+ myId);
+
+// var myNumOne = 5;
+// var myNumTwo = "5";
+// console.log(myNumOne +  parseInt(myNumTwo) );
+// console.log(myNumOne +  Number(myNumTwo) );
+// console.log(myNumOne +  +myNumTwo);
+
+
+// var myName;  //Declaration
+// myName = "khalid" //Initialization
+// console.log(myName);
+
+
+// var msgTo = "Thanks for applying here"
+// alert(msgTo)
+// document.write(msgTo);
+
+
+// var myNum = 10;
+
+// console.log(25 + myNum);
+
+
+// BODMAS
+// bracket On
+// Divison
+// Multiplication
+// Adition
+// Subtraction
+
+
+// var firstNum = 34;
+// var secNum = 25;
+// console.log(10 + firstNum - secNum + 5);  
+
+
+// var firstNum = 10;
+// var secNum = 5;
+// console.log(2 + (firstNum - secNum) + 5 * 3 * 5 / secNum);
+
+// var firstNum = 100;
+// var secNum = 5;
+// console.log(2 + (firstNum - secNum) + 5 * 2 * 3 * 15 / secNum);
+
+// // Modulus %
+// var num = 3;
+// console.log(18 % num);
+
+// Lecture # 04
+
+// var num1 = 5;
+// var num2 = "6"
+// console.log(++num1 + 5 + num2);
+
+
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost)
+
+// let myName = "Muskan"
+// const myName3 = "Ahmed"
+// myName3 = "talha"
+
+// console.log(myName2 + " is going to get married with + " +myName);
 
 
 
